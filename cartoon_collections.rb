e56534af0d280644782c0bd1b63702bb94ab973f
Stef Spears <spears.sj87@gmail.com>
2019-08-25 16:36:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{ |dwarf, i| puts "#{i+1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+ array.collect do |call|
+   call.capitalize! + "!"
+ end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  overlap = array & cheese_types
+  if overlap.empty? == true
+    return nil
+  else return overlap[0]
+  end
 end
