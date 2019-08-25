f443f59ab58493637290ef0581640b2124ca8bbc
Stef Spears <spears.sj87@gmail.com>
2019-08-25 16:19:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,11 +15,11 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.any? do |cheese| 
-    (cheese_types).include?(cheese) 
-    return cheese
-end
-return "nil"
-end
+#def find_the_cheese(array)
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  if array.any? do |cheese| 
+ #   (cheese_types).include?(cheese) 
+#    return cheese
+#end
+  #return "nil"
+#end
